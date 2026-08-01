3481889417022ce2b8b3d48c6f6959f1befced0c
n-ohama <hoge.tbone@gmail.com>
2026-08-01 20:00:21 +0900

    anyway

--- app/Timeline.tsx
+++ app/Timeline.tsx
@@ -4,7 +4,11 @@ import { useState } from "react";
 import { supabase } from "./supabase";
 import { useRouter } from "next/navigation";
 
-type Props = { user_id: string; posts: any[]; }
+type Props = {
+  user_id: string;
+  posts: any[];
+}
+
 export default function Timeline({user_id, posts}: Props) {
   const router = useRouter();
   const [inputContent, setInputContent] = useState("");
@@ -90,8 +94,24 @@ export default function Timeline({user_id, posts}: Props) {
                 </div>
                 <div className="flex gap-6 text-sm text-slate-400">
                   <span>💬 {post.stats?.comments}</span>
-                  <span>🔁 {post.stats?.reposts}</span>
-                  <span>❤️ {post.stats?.likes}</span>
+                  {/* <span>🔁 {post.stats?.reposts}</span> */}
+                  <button
+                    type="button"
+                    className="transition hover:text-slate-200"
+                    onClick={async () => {
+                      const isLiked = post.likes_uid.some((like: any) => like.user_id === user_id);
+                      
+                      if (isLiked) {
+                        await supabase.from("likes").delete().eq("post_id", post.id).eq("user_id", user_id);
+                      } else {
+                        await supabase.from("likes").insert({ post_id: post.id, user_id: user_id });
+                      }
+
+                      router.refresh();
+                    }}
+                  >
+                    ❤️ {post.likes_count}
+                  </button>
                 </div>
               </article>
             ))}
